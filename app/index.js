0e038f311600e65ba03b9f7baeb958e9e7f464e9
Michał Budzyński <michal@virtualdesign.pl>
2015-07-03 01:01:00 +0100

    :triangular_flag_on_post: component-> app

--- app/index.js
+++ app/index.js
@@ -1,126 +1,60 @@
 'use strict';
 var util = require('util');
-var path = require('path');
 var yeoman = require('yeoman-generator');
+var fs = require('fs');
 var chalk = require('chalk');
 
-var languageChoice = {
-  js: 'JavaScript',
-  ts: 'TypeScript'
-};
+var ComponentGenerator = yeoman.generators.NamedBase.extend({
 
-var KoGenerator = yeoman.generators.Base.extend({
+  detectCodeLanguage: function() {
+    this.usesTypeScript = fs.existsSync('src/app/startup.ts');
+    this.codeFileExtension = this.usesTypeScript ? '.ts' : '.js';
+  },
+
   init: function () {
-    this.pkg = require('../package.json');
-
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        // Figure out whether we have an internet connection. If not, need to
-        // pass --offline to bower otherwise it won't fall back on cache.
-        require('dns').resolve('example.com', function(isOffline) {
-          console.log('Installing dependencies in ' + (isOffline ? 'offline' : 'online') + ' mode...');
-          if (isOffline) {
-            // Patch bowerInstall to pass --offline
-            this.bowerInstall = (function(originalFunction) {
-              return function(paths, options, cb) {
-                options = options || {};
-                options.offline = true;
-                return originalFunction.call(this, paths, options, cb);
-              };
-            })(this.bowerInstall);
-          }
-
-          this.installDependencies();
-
-          if (this.includeTests) {
-            // Install test dependencies too
-            var bowerArgs = ['install'];
-            if (isOffline) {
-              bowerArgs.push('--offline');
-            }
-            this.spawnCommand('bower', bowerArgs, { cwd: 'test' });
-          }
-        }.bind(this));
-      }
-    });
+    var codeLanguage = this.usesTypeScript ? 'TypeScript' : 'JavaScript';
+    console.log('Creating component \'' + this.name + '\' (' + codeLanguage + ')...');
+    this.componentName = this.name;
+    this.dirname = 'src/components/' + this._.dasherize(this.name) + '/';
+    this.filename = this._.dasherize(this.name);
+    this.viewModelClassName = this._.classify(this.name);
   },
 
-  askFor: function () {
-    var done = this.async();
-    this.log(this.yeoman);
-    this.log(chalk.magenta('You\'re using the fantastic Knockout app generator.'));
-
-    var prompts = [{
-      name: 'name',
-      message: 'What\'s the name of your new site?',
-      default: path.basename(process.cwd())
-    }, {
-      type: 'list',
-      name: 'codeLanguage',
-      message: 'What language do you want to use?',
-      choices: [languageChoice.js, languageChoice.ts]
-    }, {
-      type: 'confirm',
-      name: 'includeTests',
-      message: 'Do you want to include automated tests, using Jasmine and Karma?',
-      default: true
-    }];
-
-    this.prompt(prompts, function (props) {
-      this.longName = props.name;
-      this.slugName = this._.slugify(this.longName);
-      this.usesTypeScript = props.codeLanguage === languageChoice.ts;
-      this.includeTests = props.includeTests;
-      done();
-    }.bind(this));
+  template: function () {
+    var codeExtension = this.usesTypeScript ? '.ts' : '.js';
+    this.copy('view.html', this.dirname + this.filename + '.html');
+    this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
   },
 
-  templating: function () {
-    var excludeExtension = this.usesTypeScript ? '.js' : '.ts';
-    this._processDirectory('src', 'src', excludeExtension);
-    this.template('_package.json', 'package.json');
-    this.template('_bower.json', 'bower.json');
-    this.template('_gulpfile.js', 'gulpfile.js');
-    this.template('_gitignore', '.gitignore');
-    this.copy('bowerrc', '.bowerrc');
-
-    if (this.includeTests) {
-      // Set up tests
-      this._processDirectory('test', 'test', excludeExtension);
-      this.copy('bowerrc_test', 'test/.bowerrc');
-      this.copy('karma.conf.js');
-    }
-
-    // Explicitly copy the .js files used by the .ts output, since they're otherwise excluded
-    if (this.usesTypeScript) {
-      this.copy('src/app/require.config.js');
-      this._processDirectory('definitions', 'definitions');
-      
-      if (this.includeTests) {
-        this.copy('test/require.config.js');
-      }
-    }
-  },
-
-  _processDirectory: function(source, destination, excludeExtension) {
-    var root = this.isPathAbsolute(source) ? source : path.join(this.sourceRoot(), source);
-    var files = this.expandFiles('**', { dot: true, cwd: root }).filter(function(filename) {
-      return !excludeExtension || path.extname(filename) !== excludeExtension;
-    });
-
-    for (var i = 0; i < files.length; i++) {
-        var f = files[i];
-        var src = path.join(root, f);
-        if(path.basename(f).indexOf('_') == 0){
-            var dest = path.join(destination, path.dirname(f), path.basename(f).replace(/^_/, ''));
-            this.template(src, dest);
+  addComponentRegistration: function() {
+    var startupFile = 'src/app/startup' + this.codeFileExtension;
+    readIfFileExists.call(this, startupFile, function(existingContents) {
+        var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
+        if (existingRegistrationRegex.exec(existingContents)) {
+            this.log(chalk.white(this.filename) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
+            return;
         }
-        else{
-            var dest = path.join(destination, f);
-            this.copy(src, dest);
+
+        var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]',
+            regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
+            modulePath = 'components/' + this.filename + '/' + this.filename,
+            lineToAdd = 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
+            newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
+        fs.writeFile(startupFile, newContents);
+        this.log(chalk.green('   registered ') + chalk.white(this.filename) + chalk.green(' in ') + chalk.white(startupFile));
+
+        if (fs.existsSync('gulpfile.js')) {
+            this.log(chalk.magenta('To include in build output, reference ') + chalk.white('\'' + modulePath + '\'') + chalk.magenta(' in ') + chalk.white('gulpfile.js'));
         }
-    }
+    });
   }
+
 });
 
-module.exports = KoGenerator;
+function readIfFileExists(path, callback) {
+    if (fs.existsSync(path)) {
+        callback.call(this, this.readFileAsString(path));
+    }
+}
+
+module.exports = ComponentGenerator;
