5c94e37053f90827f0a795c48fe188c4d0db4cb6
Michał Budzyński <michal@virtualdesign.pl>
2015-07-03 01:01:00 +0100

    :elephant: copy test file with proper path

--- app/index.js
+++ app/index.js
@@ -23,6 +23,7 @@ var ComponentGenerator = yeoman.generators.NamedBase.extend({
   template: function () {
     this.copy('view.html', this.dirname + this.filename + '.html');
     this.copy('view.less', this.dirname + this.filename + '.less');
+    this.copy('view-test.js', 'src/' + this.filename + '-test.js');
     this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
   },
 
